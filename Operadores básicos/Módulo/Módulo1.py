543d3e9094e597286eb5616d9b2e4e195fceca6d
Bryan Zumba <99735752+Bryan-Zumba@users.noreply.github.com>
2022-03-03 19:06:15 -0500

    Módulos

--- Operadores básicos/Módulo/Módulo1.py
+++ Operadores básicos/Módulo/Módulo1.py
@@ -1,10 +1,4 @@
-#En este ejercicio, el módulo nos ayudara a identificar si un número es par o impar
-
-#Determinar si el número ingresado es par o impar
-
-num=7
-
-if(num%2==0):
-    print('El numero es par')
-else:
-    print('El numero es impar')
+a=10
+b=5
+modulo=a % b
+print(modulo)
