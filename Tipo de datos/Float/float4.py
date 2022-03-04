4f172e744e1b7d9181624ce0a8b36ced502094c3
Bryan Zumba <99735752+Bryan-Zumba@users.noreply.github.com>
2022-03-03 22:58:27 -0500

    Float

---
+++ Tipo de datos/Float/float4.py
@@ -0,0 +1,12 @@
+#Operacion para hallar el valor de "z"
+
+#El total es de 100 y tenemos el valor de las dos variables
+total=100
+x= 24.45
+y=25.40
+z=0
+
+
+z=total-x-y
+
+print("El valor de z, es:", z)
