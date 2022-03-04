1ab86d81f047b08093d61e4bcce689acf11e9d03
Bryan Zumba <99735752+Bryan-Zumba@users.noreply.github.com>
2022-03-03 19:39:56 -0500

    Potencias

---
+++ Operadores básicos/Potencia/Potencia3.py
@@ -0,0 +1,20 @@
+#Expresiones aritmeticas
+
+a=2
+b=4
+
+#Primer ejercicio
+Resultado=(a**2+b**2)
+print(Resultado)
+
+#Segundo Ejercicio
+Resultado=((a+b)**2)
+print(Resultado)
+
+#Tercer Ejercicio
+Resultado=(b**(1/3)+34)
+print(Resultado)
+
+#Cuarto Ejercicio
+Resultado=((b+34*a)**(1/3))
+print(Resultado)
