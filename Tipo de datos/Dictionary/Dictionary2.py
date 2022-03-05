28b2d6212060e1794d3aa7d0bae26434d9d24bfd
Bryan Zumba <99735752+Bryan-Zumba@users.noreply.github.com>
2022-03-04 23:48:26 -0500

    Create Dictionary2.py

---
+++ Tipo de datos/Dictionary/Dictionary2.py
@@ -0,0 +1,8 @@
+antónimos = dict({'acercar':'alejar',
+               'bonito':'feo',
+               'alto':'bajo',
+               'largo':'corto',
+               'adelantar':'atrasar',
+               'grande':'pequeño',})
+
+print (antónimos)
