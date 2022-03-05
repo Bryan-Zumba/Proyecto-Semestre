c6363dd49b567122dbdd9960e7cc138a50717a4b
Bryan Zumba <99735752+Bryan-Zumba@users.noreply.github.com>
2022-03-05 00:32:47 -0500

    For

--- Condicionales/For/For1.py
+++ Condicionales/For/For1.py
@@ -1,4 +1,5 @@
-numero = int (input ("Ingrese un numero :"))
+#Una tabla de multiplicar, usando el for
+número = int (input ("Ingrese un número :"))
 
 for i in range (1, 13):
-    print(f"{i} x {numero} = {i*numero}")
+    print(f"{i} x {número} = {i*número}")
