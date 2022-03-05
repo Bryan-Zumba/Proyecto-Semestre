cdaa3925fb1593d93d5af7d3ee7b9bb1dbdf7bdd
Bryan Zumba <99735752+Bryan-Zumba@users.noreply.github.com>
2022-03-05 00:19:01 -0500

    Dictionary

---
+++ Tipo de datos/Dictionary/Dictionary3.py
@@ -0,0 +1,5 @@
+campeón_copa_ecuador = {2010: 'Liga de Quito', 2011: 'Deportivo Quito', 2012: 'Barcelona',
+                    2013: 'Emelec', 2014: 'Emelec', 2015: 'Emelec',
+                    2016: 'Barcelona', 2017: 'Emelec', 2018: 'Liga de Quito',
+                    2019: 'Delfín',2020: 'Barcelona',2021: 'Independiente del Valle',}
+print(campeón_copa_ecuador[2013])
