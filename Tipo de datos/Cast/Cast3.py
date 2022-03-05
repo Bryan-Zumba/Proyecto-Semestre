ac8b4b9e7c1277422a299ffe73e59fa12809a0f1
Bryan Zumba <99735752+Bryan-Zumba@users.noreply.github.com>
2022-03-04 22:41:28 -0500

    Cast

---
+++ Tipo de datos/Cast/Cast3.py
@@ -0,0 +1,8 @@
+#El programa pedirá al usuario datos y calculará la edad que tebdra el año entrante, usara el int para la edad
+nombre=input('Ingrese su nombre:')
+apellido=input('Ingrese su apellido:')
+edad=int(input('Ingrese su edad:'))
+
+print('Su nombre es',nombre)
+print('Su apellido es',apellido)
+print('Su edad es:',edad+1)
