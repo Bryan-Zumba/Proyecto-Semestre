e80795c48bec634ab5119c0ffbdc9a62364bc248
Bryan Zumba <99735752+Bryan-Zumba@users.noreply.github.com>
2022-03-05 00:36:16 -0500

    For

--- Condicionales/For/For2.py
+++ Condicionales/For/For2.py
@@ -1,1 +1,11 @@
-s
+#Calcular media aritmética y suma total de "n" elementos
+
+n= int(input("Ingrese el valor de N:"))
+suma=0
+for i in range(n):
+    nota=int(input("Ingrese su nota:"))
+    suma= suma+nota
+
+print("Suma total:", suma)
+media_a =suma/n
+print("Media aritmetica:", media_a)
